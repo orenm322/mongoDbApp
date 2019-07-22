fd36677857eaefa8e1bb181f39d894bfe8ca516a
Oren Mordechai <orenmordechai@gmail.com>
2019-07-22 12:31:39 -0400

    Refactored TestController@show method

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,26 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use MongoDB;
+use MongoDB\Client;
 
 class TestController extends Controller
 {
 
-    public function show()
+    public function show(Client $client)
     {
         
         $cursor = null;
-        $client = new MongoDB\Client();
-        
-    
         $collection = $client->laravelApp->posts;
-        
-
         $cursor = $collection->find();
-
-        // foreach ($cursor as $document) {
-        //     echo "<pre>" . print_r($document, true) . "</pre>";
-        // }
 
         return view('test', ['cursor'=>$cursor]);
     }
