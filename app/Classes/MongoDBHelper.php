d253d10ff468ade6fb44b18437b86273a655327a
Oren Mordechai <orenmordechai@gmail.com>
2019-07-26 02:18:15 -0400

    Added function to convert MongoDB UTCDatetime to EST

---
+++ app/Classes/MongoDBHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Classes;
+
+use DateTimeZone;
+
+class MongoDBHelper
+{
+    public static function getLocalDatetime($utcdatetime, $timezone = 'America/New_York')
+    {
+        return $utcdatetime->toDateTime()->setTimezone(new DateTimeZone($timezone))->format('Y-m-d H:i:s');
+    }
+}
+?>
