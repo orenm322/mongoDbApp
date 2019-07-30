53e7decc1d5328c1c95848a331513341c61b5083
Oren Mordechai <orenmordechai@gmail.com>
2019-07-30 01:02:36 -0400

    Inital Auth middleware setup

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 use App\Classes\Reports;
 
 class ReportsController extends Controller {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function showGraph()
     {
         $url = Reports::getEmbeededChartURL();
