ee3e15f30ba9794cc95d4f21d50ccb467264333e
Oren Mordechai <orenmordechai@gmail.com>
2019-08-07 11:58:13 -0400

    Enabled email verification at registration

--- app/User.php
+++ app/User.php
@@ -6,14 +6,17 @@ use Illuminate\Notifications\Notifiable;
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 
-class User extends Eloquent implements AuthenticatableContract, CanResetPasswordContract
+class User extends Eloquent implements AuthenticatableContract, CanResetPasswordContract, MustVerifyEmailContract
 {
     use Authenticatable;
     use Notifiable;
     use CanResetPassword;
+    use MustVerifyEmail;
 
     protected $connection = 'mongodb';
     protected $collection = 'users';
